8c5cd33f34b6b0f12d06670b83857c9e3561b4ad
Zueon <maineon0701@gmail.com>
2022-12-28 19:51:49 +0900

    delete /

--- application/src/main/java/dev/stratospheric/config/WebMvcConfig.java
+++ application/src/main/java/dev/stratospheric/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
       .allowedOrigins("http://localhost:3000",
-        "https://www.teammade.link/")
+        "https://www.teammade.link")
       .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
       .allowedHeaders("*")
       .allowCredentials(true)
